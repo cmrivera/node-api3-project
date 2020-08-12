98be6a86b5735d745a990a4767275ff4624e6cb7
cmrivera <cmrivera0004@student.stcc.edu>
2020-08-11 21:16:21 -0400

    validate user/posts/ get requets

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,43 @@
-const express = require('express');
-
+const express = require("express");
+const Users = require("./userDb");
+const Posts = require("../posts/postDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", validateUser, (req, res) => {
+  // do your magic!
+  Users.insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the user",
+      });
+    });
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
@@ -34,14 +45,53 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+
+  Users.getById(id)
+    .then((user) => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: "invalid user id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "failed", err });
+    });
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!isEmpty(req.body)) {
+    if (!req.body.name) {
+      res.status(400).json({ message: "missing required name field" });
+    } else {
+      next();
+    }
+  } else {
+    res.status(400).json({ message: "missing user data" });
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (!isEmpty(req.body)) {
+    if (!req.body.text) {
+      res.status(400).json({ message: "missing required text field" });
+    } else {
+      req.post = {
+        ...req.body,
+        user_id: req.user.id,
+      };
+      next();
+    }
+  } else {
+    res.status(400).json({ message: "missing post data" });
+  }
+}
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 module.exports = router;
